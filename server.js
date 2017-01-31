9f4e05978dc53c54800d08ce47381f13b5f6b020
shortcircuit1024 <shortcircuit1024@gmail.com>
2017-01-30 20:41:21 -0600

    Update

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 
 var app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 
 app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-        next();
-    } else {
+    if (req.headers['x-forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
     }
 });
 
